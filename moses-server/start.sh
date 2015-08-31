77f6fec312115e57dda10bf7f17fadad8d80dce0
Andrey Yaroshenko <da3n00r@gmail.com>
2015-09-01 00:28:08 +0700

    Moses Server container

---
+++ moses-server/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Starting Moses Server on ${MOSES_PORT}"
+
+$MOSES_DIR/bin/mosesserver -f $MOSES_CONFIG_DIR/moses.ini —server-port $MOSES_PORT —server-log $MOSES_LOG_DIR/server.log
