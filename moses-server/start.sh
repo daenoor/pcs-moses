c7c0ca4c612a8ffbf7a1c7f9ea73c7c6228dcea0
Andrey Yaroshenko <da3n00r@gmail.com>
2016-03-24 11:19:35 +0700

    moses-server fixes

--- moses-server/start.sh
+++ moses-server/start.sh
@@ -2,4 +2,4 @@
 
 echo "Starting Moses Server on ${MOSES_PORT}"
 
-$MOSES_DIR/bin/mosesserver -f $MOSES_CONFIG_DIR/moses.ini —server-port $MOSES_PORT —server-log $MOSES_LOG_DIR/server.log
+$MOSES_DIR/bin/mosesserver -f $MOSES_CONFIG_DIR/moses.ini --server-port $MOSES_PORT --server-log $MOSES_LOG_DIR/server.log
